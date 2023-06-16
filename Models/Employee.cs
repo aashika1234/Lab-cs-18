0da257ec92735d9a0f2fc55d491c79e0f0d9ec2f
Aashika Dhakal <aasu@Aashikas-MacBook-Air.local>
2023-06-16 08:01:07 +0545

    initiazled

---
+++ Models/Employee.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+namespace DataAnnotation.Models
+{
+    public class Employee
+    {
+        [Required(ErrorMessage = "Id is Mandatory")]
+        public int EmployeeId { set; get; }
+
+		[Required(ErrorMessage ="Name is Mandatory")]
+        public string? EmployeeName { set; get; }
+
+        [Required(ErrorMessage = "Age is Mandatory")]
+        public string? EmployeeAge { set; get; }
+
+        [Required(ErrorMessage = "Gender is Mandatory")]
+        [StringLength(1,ErrorMessage="Euta string hal gandu")]
+        public string? EmployeeGender { set; get; }
+
+        [Required(ErrorMessage = "Email is Mandatory")]
+        public string? EmployeeEmail { set; get; }
+
+
+    }
+}
+
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace DataAnnotation.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
